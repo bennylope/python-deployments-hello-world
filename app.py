d88a96ad12cbc6311561485478eba6fb9d962025
Ben Lopatin <ben@benlopatin.com>
2022-02-26 09:40:28 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Application definition
+"""
+
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get("/")
+async def home():
+    return HTMLResponse("Hello world!")
+
+
+@app.get("/404")
+async def missing():
+    return HTMLResponse("That's gonna be a 'no' from me.", status_code=404)
